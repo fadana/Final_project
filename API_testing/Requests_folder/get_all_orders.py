14e8fdd36582dcdf6f931a8de76362a308c6706d
fadana <faderanamaria@gmail.com>
2023-03-05 13:56:32 -0800

    api upgrade

---
+++ API_testing/Requests_folder/get_all_orders.py
@@ -0,0 +1,9 @@
+def submit_order(bookid, customer_name):
+		request_body = {
+				"bookId":bookid,
+				"customerName":customer_name
+		}
+		token = generate_token()
+		header_params = {'Authorization':token}
+		response = requests.post("https://simple-books-api.glitch.me/orders",json = request_body, headers= header_params)
+		return response
