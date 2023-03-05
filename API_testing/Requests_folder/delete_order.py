eb438eafec113945567c8cc2d9c68c45cd59fa4f
fadana <faderanamaria@gmail.com>
2023-03-05 11:26:43 -0800

    updated api

--- API_testing/Requests_folder/delete_order.py
+++ API_testing/Requests_folder/delete_order.py
@@ -7,4 +7,4 @@ def delete_order(orderId):
 		token = generate_token()
 		header_params = {'Authorization': token}
 		response = requests.delete(f"https://simple-books-api.glitch.me/orders/{orderId}",headers=header_params)
-		return
+		return response
--- API_testing/tests/test_delete_order.py
+++ API_testing/tests/test_delete_order.py
@@ -4,8 +4,6 @@ from API_testing.Requests_folder.submit_order import submit_order
 
 class TestDeleteOrder:
 		def test_delete_order(self):
-			order_response = submit_order(2, "Maria Anton").json()
-			assert len(order_response['orderId']) > 0, f" {order_response}"
-			response = delete_order(submit_order(2,"Maria Anton").json())
+			response = delete_order(submit_order(1,"Maria Anton").json()['orderId'])
 			assert response.status_code == 204, f'Error expected 204, actual {response}'
 			assert response.json()==""
